caa1611946d34fc47d920faeebc261787707a11d
Sagar-333 <sagarkhanna030303@gmail.com>
2024-07-23 19:13:59 +0530

    exp change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -51,8 +51,7 @@ const About = () => {
         <h3 className='subhead-text'>Work Experience.</h3>
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
-            teaming up with smart people. Here's the rundown:
+            I've worked on multiple projects for college department and also worked with multiple college clubs enhancing by Technical as well as Soft skills.
           </p>
         </div>
 
